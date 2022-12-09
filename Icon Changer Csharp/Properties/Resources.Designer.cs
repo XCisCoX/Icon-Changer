d5625326fbbeef51ec1c5c62184b13a560655d9a
CisCo_X <Shadow.dsg11@gmail.com>
2022-12-09 22:32:59 +0330

    Edit Some Stuff

--- Icon Changer Csharp/Properties/Resources.Designer.cs
+++ Icon Changer Csharp/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace CISCO_ICON_CHANGER.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,26 +57,6 @@ namespace CISCO_ICON_CHANGER.Properties {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _50039539_134636 {
-            get {
-                object obj = ResourceManager.GetObject("50039539_134636", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] Kai_Wachi_Demons_133 {
-            get {
-                object obj = ResourceManager.GetObject("Kai_Wachi_Demons_133", resourceCulture);
-                return ((byte[])(obj));
             }
         }
     }
